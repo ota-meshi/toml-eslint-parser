3f636075e2c7b46a8cb1d511f03216ca2749434e
Yosuke Ota <otameshiyo23@gmail.com>
2022-11-18 19:23:01 +0900

    use site-kit-monaco-editor-vue (#124)

--- explorer/src/components/AstOptions.vue
+++ explorer/src/components/AstOptions.vue
@@ -1,51 +1,51 @@
 <template>
-    <label class="ast-options"
-        ><input
-            type="checkbox"
-            :checked="modelValue.showLocations"
-            @change="updateShowLocations($event.target.checked)"
-        />Locations</label
-    >
-    <label class="ast-options"
-        ><input
-            type="checkbox"
-            :checked="modelValue.showValues"
-            @change="updateShowValues($event.target.checked)"
-        />Show Result JSON</label
-    >
+  <label class="ast-options"
+    ><input
+      type="checkbox"
+      :checked="modelValue.showLocations"
+      @change="updateShowLocations($event.target.checked)"
+    />Locations</label
+  >
+  <label class="ast-options"
+    ><input
+      type="checkbox"
+      :checked="modelValue.showValues"
+      @change="updateShowValues($event.target.checked)"
+    />Show Result JSON</label
+  >
 </template>
 
 <script>
 export default {
-    name: "AstOptions",
-    props: {
-        modelValue: {
-            type: Object,
-            default() {
-                return {}
-            },
-        },
+  name: "AstOptions",
+  props: {
+    modelValue: {
+      type: Object,
+      default() {
+        return {};
+      },
     },
-    emits: ["update:modelValue"],
-    methods: {
-        updateShowLocations(newValue) {
-            this.$emit("update:modelValue", {
-                ...this.modelValue,
-                showLocations: newValue,
-            })
-        },
-        updateShowValues(newValue) {
-            this.$emit("update:modelValue", {
-                ...this.modelValue,
-                showValues: newValue,
-            })
-        },
+  },
+  emits: ["update:modelValue"],
+  methods: {
+    updateShowLocations(newValue) {
+      this.$emit("update:modelValue", {
+        ...this.modelValue,
+        showLocations: newValue,
+      });
     },
-}
+    updateShowValues(newValue) {
+      this.$emit("update:modelValue", {
+        ...this.modelValue,
+        showValues: newValue,
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
 .ast-options {
-    padding-left: 16px;
+  padding-left: 16px;
 }
 </style>
--- explorer/vue.config.js
+++ explorer/vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   publicPath: "/toml-eslint-parser/",
+  transpileDependencies: [/site-kit-monaco-editor-vue/],
+  chainWebpack(config) {
+    config.resolve.extensions.prepend(".ts");
+
+    config.module
+      .rule("ts")
+      .test(/\.ts$/)
+      .use("babel-loader")
+      .loader("babel-loader");
+  },
 };
