f28db751c639f74634cd532f6d9f2eb8930bceff
ota-meshi <otameshiyo23@gmail.com>
2023-05-09 11:52:02 +0900

    chore: fix

--- tools/lib/changesets-util.ts
+++ tools/lib/changesets-util.ts
@@ -5,6 +5,6 @@ import path from "path";
 export async function getNewVersion(): Promise<string> {
   const releasePlan = await getReleasePlan(path.resolve(__dirname, "../.."));
 
-  return releasePlan.releases.find(({ name }) => name === "yaml-eslint-parser")!
+  return releasePlan.releases.find(({ name }) => name === "toml-eslint-parser")!
     .newVersion;
 }
