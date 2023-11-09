9e19008962c138fc774e4eea676a8b6b03280e99
ota-meshi <otameshiyo23@gmail.com>
2023-11-09 22:29:08 +0900

    fix

--- toml-test-decode-toml1.1.js
+++ toml-test-decode-toml1.1.js
@@ -5,7 +5,7 @@
 const fs = require("fs");
 /* eslint n/no-missing-require: off -- ignore */
 const toml = require("./lib/index.js");
-const { convertTomlTestValue } = require("./toml-test-decode.js");
+const { convertTomlTestValue } = require("./toml-test-decode-util.js");
 
 const ast = toml.parseTOML(fs.readFileSync(0, "utf-8"), { tomlVersion: "1.1" });
 const result = `${JSON.stringify(convertTomlTestValue(ast), null, 2)}\n`;
