1efaf172c025f0b6b38f05245b50c68de6ab77fa
Yosuke Ota <otameshiyo23@gmail.com>
2026-02-06 01:55:52 +0000

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -39,6 +39,18 @@ export default [
     },
   },
   {
+    files: ["**/*.{js,ts,mjc,mts,cjs,cts}"],
+    rules: {
+      "n/prefer-node-protocol": "error",
+      "n/file-extension-in-import": ["error", "always"],
+    },
+    settings: {
+      n: {
+        typescriptExtensionMap: [],
+      },
+    },
+  },
+  {
     files: ["explorer/**/*.js"],
     languageOptions: {
       sourceType: "script",
